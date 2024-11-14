a7488d30ee7b43c299ffa73707b72f1a59270070
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-14 14:17:02 -0300

    Add files via upload

---
+++ Ex1-AreaRetangulo/Lista4POO-hiago/AreaRetangulo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4POO_hiago
+{
+    internal class AreaRetangulo
+    {
+        private int n1;
+        private int n2;
+        private int resultado;
+
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+        public void setN2(int n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void CalcularArea(int n1, int n2)
+        {
+            this.resultado = n1 * n2;
+        }
+
+        public int getArea()
+        {
+            return this.resultado;
+        }
+    }
+}
---
+++ Ex1-AreaRetangulo/Lista4POO-hiago/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista4POO_hiago
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, resultado;
+            AreaRetangulo ar;
+            ar = new AreaRetangulo();
+
+            Console.WriteLine("Calcule a área de um retângulo.");
+            
+            Console.WriteLine("Insira base: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira altura: ");
+            n2 = int.Parse(Console.ReadLine());
+            
+            ar.CalcularArea(n1,n2);
+
+            Console.WriteLine("Area = {0}",ar.getArea() );
+
+
+        }
+    }
+}
