be2eb4aa0b703fcc32f19dfc1814977bd2cef9aa
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 12:09:30 -0300

    Add files via upload

---
+++ Ex6-MilhaMarMetro/Ex6-MilhaMarMetro/Milhas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_MilhaMarMetro
+{
+    internal class Milhas
+    {
+        private float n1;
+        private float resultado;
+
+        public void setN1(float n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void CalcularDistancia(float n1)
+        {
+            this.resultado = (n1*1852)/1000; 
+        }
+
+        public float getDistancia()
+        {
+            return this.resultado;
+        }
+    }
+}
---
+++ Ex6-MilhaMarMetro/Ex6-MilhaMarMetro/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_MilhaMarMetro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1;
+            Milhas m = new Milhas();
+            Console.WriteLine("Coverta milhas para metros");
+            Console.Write("Digite milha: ");
+            n1 = int.Parse(Console.ReadLine());
+            m.CalcularDistancia(n1);
+            Console.WriteLine("Resultado: {0} em milhas equivale á {1} quilómetros",n1, m.getDistancia());
+
+        }
+    }
+}
