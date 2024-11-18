fb0dcfa21ed090e0db550320291cfb767b6ca91d
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 15:07:31 -0300

    Add files via upload

---
+++ Ex8-MaiorNum/Ex8-MaiorNum/Maior.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8_MaiorNum
+{
+    internal class Maior
+    {
+        private int n1;
+        private int n2;
+        private int resultado;
+
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void setN2(int n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void CalcularMaior(int n1, int n2)
+        {
+            if(n1 > n2)
+            {
+                this.resultado = n1;
+            }else if(n1 < n2)
+            {
+                this.resultado = n2;
+            }
+        }
+
+        public int getMaior()
+        {
+            return this.resultado;
+        }
+    }
+}
---
+++ Ex8-MaiorNum/Ex8-MaiorNum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8_MaiorNum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2;
+            Maior m = new Maior();
+
+            Console.WriteLine("Insira dois numeros e calcule o maior entre eles");
+            Console.Write("Digite o 1º valor: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.Write("Digite o 2º valor: ");
+            n2 = int.Parse(Console.ReadLine());
+
+            m.CalcularMaior(n1, n2);
+            Console.WriteLine("O maior número é {0}", m.getMaior());
+
+        }
+    }
+}
