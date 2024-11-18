153b500005f7196e4489d7f007ca2bc4ce89d674
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 15:58:00 -0300

    Add files via upload

---
+++ Ex9-MaiorNumORIgual/Ex9-MaiorNumORIgual/Maior.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex9_MaiorNumORIgual
+{
+    internal class Maior
+    {
+        private int n1;
+        private int n2;
+        private string resultado;
+
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void setN2(int n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void CalcularMaior()
+        {
+            if (n1 > n2)
+            {
+                this.resultado = n1.ToString();
+            }
+            else if (n1 < n2)
+            {
+                this.resultado = n2.ToString();
+            }
+            else if (n1 == n2)
+            {
+                this.resultado = "Igual";
+            }
+
+        }
+
+        public string getMaior()
+        {
+            return this.resultado;
+        }
+    }
+}
---
+++ Ex9-MaiorNumORIgual/Ex9-MaiorNumORIgual/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("Ex9-MaiorNumORIgual")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Ex9-MaiorNumORIgual")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a28c7e62-f3df-486e-9e18-b4aa1e53240a")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
