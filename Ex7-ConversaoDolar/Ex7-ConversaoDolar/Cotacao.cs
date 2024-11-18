ece6103812a11c14a7ae85b2b5127922c0527317
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 14:28:39 -0300

    Add files via upload

---
+++ Ex7-ConversaoDolar/Ex7-ConversaoDolar/Cotacao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7_ConversaoDolar
+{
+    internal class Cotacao
+    {
+        private float n1;
+        private float n2;
+        private float resultado;
+
+        public void setN1(float n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void setN2(float n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void CalcularCotacao(float n1, float n2)
+        {
+            this.resultado = n1 * n2;
+        }
+
+        public float getCotacao()
+        {
+            return this.resultado;
+        }
+    }
+}
---
+++ Ex7-ConversaoDolar/Ex7-ConversaoDolar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex7_ConversaoDolar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float n1, n2;
+            Cotacao c = new Cotacao();
+
+            Console.WriteLine("Insira a cotaçao e converta dolares para reais");
+            Console.Write("Digite cotação do dolar: ");
+            n1 = float.Parse(Console.ReadLine());
+            Console.Write("Digite valor em dolares: ");
+            n2 = float.Parse(Console.ReadLine());
+
+            c.CalcularCotacao(n1, n2);
+            Console.WriteLine("Resultado: {0} USD com cotação de {1} equivale a {2} BRL", n2, n1, c.getCotacao());
+        }
+    }
+}
