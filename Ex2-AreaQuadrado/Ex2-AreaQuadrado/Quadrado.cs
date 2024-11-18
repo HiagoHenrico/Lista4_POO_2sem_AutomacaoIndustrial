eec0efaae0b98cc5ffe40ad553a5ab6bc8bbce47
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-18 00:06:39 -0300

    Add files via upload

---
+++ Ex2-AreaQuadrado/Ex2-AreaQuadrado/Quadrado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2_AreaQuadrado
+{
+    internal class Quadrado
+    {
+        private int n1;
+        private int resultado;
+
+        public void setN1(int n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void CalcularArea(int n1)
+        {
+            this.resultado = n1 * n1;
+        }
+
+        public int getArea()
+        {
+            return this.resultado;
+        }
+    }
+}
