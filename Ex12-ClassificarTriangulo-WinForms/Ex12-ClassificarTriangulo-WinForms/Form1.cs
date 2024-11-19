df51b41603420ecf1ce4d8ac47dd19e36d0014cd
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-19 16:55:36 -0300

    Add files via upload

---
+++ Ex12-ClassificarTriangulo-WinForms/Ex12-ClassificarTriangulo-WinForms/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex12_ClassificarTriangulo_WinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Classificar_Click(object sender, EventArgs e)
+        {
+            Triangulo t = new Triangulo();
+            t.SetLadoA(double.Parse(A.Text));
+            t.SetLadoB(double.Parse(B.Text));
+            t.SetLadoC(double.Parse(C.Text));
+            t.EhTriangulo();
+            label4.Text = t.Classificar().ToString();
+        }
+    }
+}
