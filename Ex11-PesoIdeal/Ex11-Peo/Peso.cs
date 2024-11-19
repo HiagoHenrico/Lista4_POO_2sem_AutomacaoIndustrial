8e98a0ca4fd747bfda9a72e514163bcf9ecd4edb
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-19 16:15:54 -0300

    Add files via upload

---
+++ Ex11-PesoIdeal/Ex11-Peo/Peso.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_Peo
+{
+    internal class Peso
+    {
+        private double n1;
+        private double n2;
+        private double peso;
+
+        public void setN1(double n1)
+        {
+            this.n1 = n1;
+        }
+
+        public void setN2(double n2)
+        {
+            this.n2 = n2;
+        }
+
+        public void PesoCidadao()
+        {
+            this.peso = n1 / (n2*n2);
+        }
+
+        public string CalcularPesoIdeal()
+        {
+            if(peso < 20)
+            {
+                return "Abaixo do peso";
+            }else if(peso >= 20 && peso < 25){
+                return "Peso Ideal";
+            }else
+            {
+                return "Acima do Peso";
+            }
+        }
+
+        public double getPeso()
+        {
+            return this.peso;
+        }
+    }
+}
---
+++ Ex11-PesoIdeal/Ex11-Peo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex11_Peo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double n1, n2;
+            Peso p = new Peso();
+            Console.WriteLine("Calcule o peso ideal de um indivíduo");
+
+            Console.WriteLine("Digite o peso em KG: ");
+            n1 = Convert.ToDouble(Console.ReadLine());
+            p.setN1(n1);
+
+            Console.Write("Digite a altura em M: ");
+            n2 = Convert.ToDouble(Console.ReadLine());
+            p.setN1(n2);
+            p.CalcularPesoIdeal();
+            Console.Write("Peso: {0} - Altura: {1}", n1, n2);
+            Console.Write(" Você está: " + p.getPeso());
+
+        }
+    }
+}
+
