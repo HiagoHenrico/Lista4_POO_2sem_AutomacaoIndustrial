41089998018c2075b52ec891a08b6b7302d0f335
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-19 16:37:07 -0300

    Add files via upload

---
+++ Ex12-DefinaOTriangulo/Ex12-DefinaOTriangulo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12_DefinaOTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+
+            Console.WriteLine("Digite os três lados de um triângulo e depois classifique-o");
+
+            Console.WriteLine("Digite o valor do lado A:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor do lado A:");
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor do lado A:");
+            double C = Convert.ToDouble(Console.ReadLine());
+
+            t.SetLados(A, B, C);
+
+            if (t.EhTriangulo())
+            {
+                Console.WriteLine("Os valores formam um triângulo.");
+                Console.WriteLine("Classificação: " + t.Classificar());
+            }
+            else
+            {
+                Console.WriteLine("Os valores não formam um triângulo.");
+            }
+
+        } 
+    }
+}
+    
+
---
+++ Ex12-DefinaOTriangulo/Ex12-DefinaOTriangulo/Triangulo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12_DefinaOTriangulo
+{
+    internal class Triangulo
+    {
+        private double A;
+        private double B;
+        private double C;
+
+        public void SetLados(double A, double B, double C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+
+        public bool EhTriangulo()
+        {
+            return (A + B > C) &&
+                   (A + C > B) &&
+                   (B + C > A);
+        }
+
+        public string Classificar()
+        {
+            if (A == B && B == C)
+            {
+                return "Equilátero";
+            }
+            else if (A == B || A == C || B == C)
+            {
+                return "Isósceles";
+            }
+            else
+            {
+                return "Escaleno";
+            }
+        }
+    }
+}
