a71ad83667473147cb1f1a1bff99651e3c737180
Hiago Henrico <96848900+HiagoHenrico@users.noreply.github.com>
2024-11-19 17:05:53 -0300

    Add files via upload

---
+++ Ex13-TrianguloRetangulo/Ex13-TrianguloRetangulo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex13_TrianguloRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrianguloRetangulo triangulo = new TrianguloRetangulo();
+
+            Console.WriteLine("Digite o valor do lado A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor do lado B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Digite o valor do lado C:");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            triangulo.SetLados(a, b, c);
+
+            if (triangulo.EhTrianguloRetangulo())
+            {
+                Console.WriteLine("Os valores formam um triângulo retângulo.");
+            }
+            else
+            {
+                Console.WriteLine("Os valores não formam um triângulo retângulo.");
+            }
+        }
+    }
+}
---
+++ Ex13-TrianguloRetangulo/Ex13-TrianguloRetangulo/TrianguloRetangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex13_TrianguloRetangulo
+{
+    internal class TrianguloRetangulo
+    {
+        private double a;
+        private double b;
+        private double c;
+
+        public void SetLados(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public bool EhTrianguloRetangulo()
+        {
+            double[] lados = { a, b, c };
+            Array.Sort(lados); 
+
+            double hipotenusa = lados[2];
+            double cateto1 = lados[0];
+            double cateto2 = lados[1];
+
+            return Math.Pow(hipotenusa, 2) == Math.Pow(cateto1, 2) + Math.Pow(cateto2, 2);
+        }
+    }
+}
+
